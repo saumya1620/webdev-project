bf81b75d8ff7d086dabc2dc2d612aa679e5ba43e
Sarthak Mishra <sarthakjm2284@gmail.com>
2025-08-16 13:20:28 +0530

    Added team controller with a basic route

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, 
-{ timestamps: true });
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
+}, { timestamps: true });
 module.exports = mongoose.model('Team', teamSchema);
