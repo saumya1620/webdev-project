00177c955e262da5b74fb6dfb724ea198534abdf
=saamya <=saamyasingla24@gmail.com>
2025-08-19 10:01:15 +0530

    final fixes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,10 +5,7 @@ const { registerUser, loginUser, getcurrent } = require("../controllers/authCont
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser);
-
-
 router.post("/login", loginUser);
-
 router.get("/info",  getcurrent);
 
 module.exports = router;
